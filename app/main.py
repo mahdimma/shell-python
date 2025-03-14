db6719fbcbba553434bab1b912f16d4d3e1fa788
mahdi <mahdiabbasi602@gmail.com>
2025-03-14 07:13:10 +0330

    type exe

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -11,11 +12,17 @@ def main():
         elif command_expand[0] == "echo":
             print(" ".join(command_expand[1:]))
         elif command_expand[0] == "type":
+            PATH = os.environ.get("PATH", "")
             match command_expand[1]:
                 case "echo" | "exit" | "type":
                     print(f"{command_expand[1]} is a shell builtin")
                 case _:
-                    print(f"{command_expand[1]}: not found")
+                    for dir in PATH.split(":"):
+                        if os.path.exists(f"{dir}/{command_expand[1]}"):
+                            print(f"{command_expand[1]} is {dir}/{command_expand[1]}")
+                            break
+                    else:
+                        print(f"{command_expand[1]}: not found")
         else:
             print(f"{command}: command not found")
 
