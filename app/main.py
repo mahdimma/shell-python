8e2b0040195418be9ad2e7404b7dab8c933337f5
mahdi <mahdiabbasi602@gmail.com>
2025-03-14 07:27:27 +0330

    run exe

--- app/main.py
+++ app/main.py
@@ -3,28 +3,40 @@ import os
 
 
 def main():
+    PATH = os.environ.get("PATH", "")
     while True:
         sys.stdout.write("$ ")
         command = input()
         command_expand = command.split()
-        if command_expand[0] == "exit":
-            break
-        elif command_expand[0] == "echo":
-            print(" ".join(command_expand[1:]))
-        elif command_expand[0] == "type":
-            PATH = os.environ.get("PATH", "")
-            match command_expand[1]:
-                case "echo" | "exit" | "type":
-                    print(f"{command_expand[1]} is a shell builtin")
-                case _:
-                    for dir in PATH.split(":"):
-                        if os.path.exists(f"{dir}/{command_expand[1]}"):
-                            print(f"{command_expand[1]} is {dir}/{command_expand[1]}")
-                            break
-                    else:
-                        print(f"{command_expand[1]}: not found")
-        else:
-            print(f"{command}: command not found")
+        match command_expand[0]:
+            case "exit":
+                break
+            case "echo":
+                print(" ".join(command_expand[1:]))
+            case "type":
+                match command_expand[1]:
+                    case "echo" | "exit" | "type":
+                        print(f"{command_expand[1]} is a shell builtin")
+                    case _:
+                        notFound = True
+                        for dir in PATH.split(":"):
+                            if os.path.exists(f"{dir}/{command_expand[1]}"):
+                                print(
+                                    f"{command_expand[1]} is {dir}/{command_expand[1]}"
+                                )
+                                notFound = False
+                                break
+                        if notFound:
+                            print(f"{command_expand[1]}: not found")
+            case _:
+                notFound = True
+                for dir in PATH.split(":"):
+                    if os.path.exists(f"{dir}/{command_expand[0]}"):
+                        os.system(f"{command}")
+                        notFound = False
+                        break
+                if notFound:
+                    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
